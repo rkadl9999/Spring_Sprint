255d9062ac9415439417bbd957ffe811aaae3a19
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-05-07 14:25:16 +0900

    ✨ :: Add HelloControllerTest

---
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/test/java/com/hyeonuk/springboot/web/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.hyeonuk.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void return_hello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
