b70c3ac2ddaf0defb85f7e933d709764a1de9cab
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-05-10 11:17:58 +0900

    ✨ :: Add Posts 등록 API

---
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/java/com/hyeonuk/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.hyeonuk.springboot.service.posts;
+
+import com.hyeonuk.springboot.domain.posts.PostsRepository;
+import com.hyeonuk.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
