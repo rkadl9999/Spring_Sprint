cc966d1bddeb96052d2782ee66fb6adc4f6b103b
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-05-10 09:10:07 +0900

    ✨ :: Add PostsRepositoryTest

---
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/test/java/com/hyeonuk/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.hyeonuk.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시물_저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("s21051@gsm.hs.kr")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
