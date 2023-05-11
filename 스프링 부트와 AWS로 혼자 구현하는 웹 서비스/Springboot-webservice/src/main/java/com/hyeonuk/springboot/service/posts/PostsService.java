92136880e10722971fa7f78214b76354f08f5df5
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-05-11 11:18:45 +0900

    👍 :: 게시물 수정 && 조회 기능 추가

--- 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/java/com/hyeonuk/springboot/service/posts/PostsService.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/java/com/hyeonuk/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.hyeonuk.springboot.service.posts;
 
+import com.hyeonuk.springboot.domain.posts.Posts;
 import com.hyeonuk.springboot.domain.posts.PostsRepository;
+import com.hyeonuk.springboot.web.dto.PostsResponseDto;
 import com.hyeonuk.springboot.web.dto.PostsSaveRequestDto;
+import com.hyeonuk.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
     }
 }
