a9c31bfc123a5a8f54009a990d3b56a7b9584300
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-05-13 23:19:51 +0900

    👍 :: 게시물 조회 페이지 구현

--- 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/java/com/hyeonuk/springboot/service/posts/PostsService.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/java/com/hyeonuk/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hyeonuk.springboot.service.posts;
 
 import com.hyeonuk.springboot.domain.posts.Posts;
 import com.hyeonuk.springboot.domain.posts.PostsRepository;
+import com.hyeonuk.springboot.web.dto.PostsListResponseDto;
 import com.hyeonuk.springboot.web.dto.PostsResponseDto;
 import com.hyeonuk.springboot.web.dto.PostsSaveRequestDto;
 import com.hyeonuk.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,6 +31,13 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id) {
--- 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/java/com/hyeonuk/springboot/web/IndexController.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/java/com/hyeonuk/springboot/web/IndexController.java
@@ -1,14 +1,19 @@
 package com.hyeonuk.springboot.web;
 
+import com.hyeonuk.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
