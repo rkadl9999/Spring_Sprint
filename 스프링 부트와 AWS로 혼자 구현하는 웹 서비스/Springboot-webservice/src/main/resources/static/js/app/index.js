4b06ff7c771c36e7967f5691b341386380231afd
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-06-09 08:56:44 +0900

    👍 :: 게시물 삭제 기능 구현

--- 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/resources/static/js/app/index.js
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/Springboot-webservice/src/main/resources/static/js/app/index.js
@@ -9,6 +9,10 @@ let main = {
         $('#btn-update').on('click', function () {
             _this.update();
         });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        })
     },
     save : function () {
         let data = {
@@ -46,6 +50,21 @@ let main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        let id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
